c56babe5652c5c7d73a35068c8c9181f1d26b0ba
Hoàng Tâm <59590158+phtam@users.noreply.github.com>
2020-08-31 23:05:20 +0700

    add script

---
+++ Azure_Assignment/Scripts/palkia-js/Palkia-config.js
@@ -0,0 +1,27 @@
+﻿// File input
+$(function () {
+    bsCustomFileInput.init();
+});
+// Active Slidebar
+$(function () {
+    var href = window.location.href;
+    $('nav ul li a').each(function (e, i) {
+        if (href.indexOf($(this).attr('href')) >= 0) {
+            $(this).addClass('active');
+        }
+    });
+});
+// Summernote
+$(function () {
+    $('#summernote').summernote()
+    $('#summernote_Specification').summernote()
+})
+
+$("input[data-bootstrap-switch]").each(function () {
+    $(this).bootstrapSwitch('state', $(this).prop('checked'));
+});
+
+$('#txa_display_Specification').summernote('disable');
+$('#txa_display_Description').summernote('disable');
+$('#txa_display_Blog_Content').summernote('disable');
+$('#txa_display_Blog_Comment').summernote('disable');
